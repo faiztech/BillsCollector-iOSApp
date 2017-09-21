f6a1fad19511b460080f45d2cfe7fe26d4bfd634
Mohammed Faiz <mfaizudd@neiu.edu>
2017-09-21 15:56:39 -0500

    addTapped done, first Screen done, coreData done

--- BillsCollector/BillsViewController.swift
+++ BillsCollector/BillsViewController.swift
@@ -28,13 +28,40 @@ class BillsViewController: UIViewController, UIImagePickerControllerDelegate, UI
         
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        
+        let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        imageView.image = image
+        imagePicker.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+        
+        
+        
+        
+        
         
     }
     
-    @IBAction func cameraTapped(_ sender: Any) {
-    }
-    
     @IBAction func addTapped(_ sender: Any) {
+        
+       let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let bill = Bill(context:context)
+        
+        bill.title = textField.text
+         bill.image = UIImagePNGRepresentation(imageView.image!)
+        
+        (UIApplication.shared.delegate as! AppDelegate).saveContext()
+        
+        
+        //pop back
+        navigationController!.popViewController(animated: true)
     }
     
 }
--- BillsCollector/ViewController.swift
+++ BillsCollector/ViewController.swift
@@ -8,18 +8,53 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var bills :[Bill] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+         let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        do
+        {
+            bills = try context.fetch(Bill.fetchRequest())
+            print(bills)
+            tableView.reloadData()
+            
+        } catch
+        {
+            
+        }
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return bills.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = UITableViewCell()
+        let bill  = bills[indexPath.row]
+        cell.textLabel?.text = bill.title
+        
+        cell.imageView?.image = UIImage(data: bill.image as Data!)
+        
+        
+        
+        return cell
+        
+    }
+  
 
 }
 
