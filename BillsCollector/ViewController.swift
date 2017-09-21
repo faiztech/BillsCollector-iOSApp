285cfed8315a80589fa563c3f3bbd8ddb78200ed
Mohammed Faiz <mfaizudd@neiu.edu>
2017-09-21 11:29:11 -0500

    Initial Commit

---
+++ BillsCollector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BillsCollector
+//
+//  Created by Mohammed Faizuddin on 9/21/17.
+//  Copyright © 2017 Faiz Tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
